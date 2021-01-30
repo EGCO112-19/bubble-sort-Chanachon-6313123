8717627ba4af8f189d60613fd7dcac75fdd7efad
Chanachon631312 <chanachon.jan@student.mahidol.edu>
2021-01-30 05:59:53 +0000

    test1

--- bubble.h
+++ bubble.h
@@ -20,14 +20,22 @@ void display(int value[], int N){
 void bubble(int a[], int N){
     int i;
     int j;
-    
-    for(j=0;j<N-1;j++){
-        if(a[j]>a[j+1]) // decending
+    int sorted;
+  for(i=N-1;i>0;i--){
+    sorted=0;
+    for(j=0;j<i;j++){
+        if(a[j]>a[j+1]) {// decending
             swap(&a[j],&a[j+1]);
+            sorted=1;
+        }
         display(a,N);
     }
     
+    printf("\t===end of round %d===\n",i);
+    if (sorted==0) break;
+  }
     
 }
+
 
 #endif /* bubble_h */
--- simpleMain.c
+++ simpleMain.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "bubble.h"
+#include "selection.h"
+#include "insertion.h"
 
-void main(int argc, char **argv){
-  printf("There are %d arguments\n",argc-1);
-  int i;
-  for(i=1;i<argc;i++)
-    printf("argument#%d is %d\n",i, atoi(argv[i]));
-    //55
-    //555
+int main(int argc, char *argv[]){
+
+  int i,N;
+  printf("There are %d arguments\n",argc);
+  for(i=0;i<argc;i++){
+         printf("argv %d: %s\n",i,argv[i]); 
+  }
+
+  N=argc-1;
+
+  int *a;
+  a=(int*) malloc(sizeof(int)*N);
+
+  for(i=1;i<argc;i++){
+        a[i-1]=atoi(argv[i]);
+  }
+  
 
 
-
+  //  bubble(a,N);
+ // insertion(a,N);
+    selection(a,N);
+ 
+  return 0;
 }
