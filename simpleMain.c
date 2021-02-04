8ee47c006e80bd4062ae500ebb3c8b2ba481816e
Chanachon631312 <chanachon.jan@student.mahidol.edu>
2021-02-04 01:28:44 +0000

    test 5

--- simpleMain.c
+++ simpleMain.c
@@ -11,19 +11,34 @@ int main(int argc, char *argv[]){
 
   printf("There are %d arguments\n",argc);
   for(i=0;i<argc;i++){
-         printf("argv %d: %s\n",i,argv[i]); 
+         printf("argv %d: %s\n",i,argv[i]);
+         //N=*argv[i]; 
   }
 
+
   N=argc-1;
+  //N=*argv[i-1];
   int *a;
 
+ a=(int*) malloc(sizeof(int)*N);
+
+  for(i=1;i<argc;i++){
+        a[i-1]=atoi(argv[i]);
+        
+  }
+
   if(strcmp(argv[1],"bubble")==0){
+    //argv[1]=NULL;
   bubble(a,N);
   }
 
   if(strcmp(argv[1],"insertion")==0){
+    //argv[1]=NULL;
   insertion(a,N);
   }
+
+
+
   
   /*a=(int*) malloc(sizeof(int)*N);
 
